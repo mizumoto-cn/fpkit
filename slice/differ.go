db3a878aa46a26d115a8a62328854cfa77d7d110
Mizumoto <mizumotokunn@gmail.com>
2024-07-02 20:19:16 +0900

    copyright

--- slice/differ.go
+++ slice/differ.go
@@ -17,19 +17,3 @@
  * limitations under the License.
  */
 package slice
-
-import (
-	"github.com/mizumoto-cn/fpkit/internal/err"
-)
-
-func Insert[T any](s []T, index int, value T) ([]T, error) {
-	if index < 0 || index > len(s) {
-		return nil, err.NewIndexOutOfRangeError(index, len(s))
-	}
-	var zeroValue T
-	s = append(s, zeroValue)
-	// `copy` operates the memory directly, so it is faster than a for loop
-	copy(s[index+1:], s[index:])
-	s[index] = value
-	return s, nil
-}
