ac62bfe13840a1757c336996af0603ed9cc4dc58
Mizumoto <mizumotokunn@gmail.com>
2024-07-02 22:33:12 +0900

    bug fix

--- main.go
+++ main.go
@@ -18,18 +18,6 @@
  */
 package main
 
-import (
-	"slices"
-
-	"github.com/mizumoto-cn/fpkit/functional"
-)
-
 func main() {
 
-	println(functional.Sum(1, 2, 3+4i))
-
-	mlist := []int{}
-
-	m := slices.Max[[]int](mlist)
-	print(m)
 }
